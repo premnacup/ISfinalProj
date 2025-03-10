2b3a8846a776966414cc153443be7cd2e7fb2d05
premnacup <premprangja@gmail.com>
2025-03-10 22:23:07 +0700

    Add more references

--- views/reference_page.py
+++ views/reference_page.py
@@ -8,10 +8,12 @@ st.write(
     <h2>Information</h2>
     <ul>
         <li><a href="https://www.geeksforgeeks.org/support-vector-machine-algorithm/">SVM algorithm</a></li>
+        <li><a href="https://www.geeksforgeeks.org/decision-tree-algorithm/">Decision Tree algorithm</a></li>
     </ul>
     <h2>Media</h2>
     <ul>
         <li><a href="https://media.geeksforgeeks.org/wp-content/uploads/20231109124312/Hinge-loss-(2).png">SVM visualization</a></li>
+        <li><a href="https://media.geeksforgeeks.org/wp-content/uploads/20250107141217134593/Decision-Tree.webp">Decision Tree visualization</a></li>
     </ul>
     </p>
     """,
